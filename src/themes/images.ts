4fc5e24343458b8f30ca403eb26ab006a2969f05
ujeon <wj9304@gmail.com>
2021-12-30 14:10:50 +0900

    :sparkles: 이미지 및 컬러 스키마 추가

---
+++ src/themes/images.ts
@@ -0,0 +1,15 @@
+import imgProfileOne from '../assets/img-profile-1.png';
+import imgProfileTwo from '../assets/img-profile-2.png';
+import imgProfileThree from '../assets/img-profile-3.png';
+import imgProfileFour from '../assets/img-profile-4.png';
+import imgProfileFive from '../assets/img-profile-5.png';
+
+const images = {
+  imgProfileOne,
+  imgProfileTwo,
+  imgProfileThree,
+  imgProfileFour,
+  imgProfileFive,
+};
+
+export default images;
